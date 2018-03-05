ee3b39a0865da0401ac3b01e8eaa919cdb1e57b3
Martin Pirklbauer Svensson <martin-1@live.se>
2018-03-05 09:13:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/martinpirklbauersv/apl/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.martinpirklbauersv.apl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText userName,password;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
